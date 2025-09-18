6b35c1059d4610bf94b175132662f58d6c62e859
Parth99001 <parthgandhi350@gmail.com>
2025-09-18 15:27:00 +0530

    proj

--- project/proj-2/index.js
+++ project/proj-2/index.js
@@ -3,7 +3,13 @@ const app = express()
 
 const PORT = 8080
 
+app.set("view engine", "ejs")
+app.use(express.urlencoded({extended:true}))
 
+let array = []
+app.get("/",(res,req) => {
+    return res.render("home", { array });
+})
 
 app.listen(PORT,(err) => {
     if(err){
