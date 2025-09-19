da4db321b1e6950aa5ca5195778b086a331763db
Parth99001 <parthgandhi350@gmail.com>
2025-09-19 15:54:11 +0530

    add

--- project/proj-2/index.js
+++ project/proj-2/index.js
@@ -7,13 +7,28 @@ app.set("view engine", "ejs")
 app.use(express.urlencoded({extended:true}))
 
 let array = []
-app.get("/",(res,req) => {
+
+app.get("/", (req, res) => {
     return res.render("home", { array });
+});
+
+app.post("/addData", (res,req)=>{
+    console.log(res.body)
+    array.push(res.body)
+//    return res.redirect("/")
 })
+
+// app.get("/delete/:id", (res,req) => {
+//     array.filter((element,index) => a)
+// })
+
+// app.get("/update/:id", (res,req) => {
+
+// })
 
 app.listen(PORT,(err) => {
     if(err){
-        console.log("server is not running")
+        console.log("server is not running",PORT)
         return
     }
     console.log("server is running",PORT)
