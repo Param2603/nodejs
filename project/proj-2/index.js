d59978c51fb7f30e3ec883b9f0da9264d0e003f6
Param2603 <ps26032005@gmail.com>
2025-09-22 23:30:17 +0530

    proj2

--- project/proj-2/index.js
+++ project/proj-2/index.js
@@ -3,33 +3,42 @@ const app = express()
 
 const PORT = 8080
 
-app.set("view engine", "ejs")
-app.use(express.urlencoded({extended:true}))
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }))
 
 let array = []
 
 app.get("/", (req, res) => {
-    return res.render("home", { array });
+  return res.render("home", { array })
 });
 
-app.post("/addData", (res,req)=>{
-    console.log(res.body)
-    array.push(res.body)
-//    return res.redirect("/")
-})
+app.post("/add-data", (req, res) => {
+  console.log(req.body)
+  array.push(req.body)
+  res.redirect("/")
+});
 
-// app.get("/delete/:id", (res,req) => {
-//     array.filter((element,index) => a)
-// })
+app.get("/delete/:id", (req, res) => {
+//   console.log(req.params.id)
+  array = array.filter((element, index) => index != req.params.id)
+  res.redirect("/")
+});
 
-// app.get("/update/:id", (res,req) => {
+app.get("/edit/:id", (req, res) => {
+  let updateData = array[req.params.id]
+  return res.render("update", { updateData, updateId: req.params.id })
+});
 
-// })
+app.post("/update-data", (req, res) => {
+  let { updateId, name, password, email, gender } = req.body
+  array[updateId] = { name, password, email, gender }
+  return res.redirect("/")
+});
 
-app.listen(PORT,(err) => {
-    if(err){
-        console.log("server is not running",PORT)
-        return
-    }
-    console.log("server is running",PORT)
-})
+app.listen(PORT, (err) => {
+  if (err) {
+    console.log("server is not running")
+    return;
+  }
+  console.log("server is running", PORT)
+});
