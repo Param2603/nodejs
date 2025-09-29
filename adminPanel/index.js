77f29065d10014b8adb1c17e5f579231bd14b134
Parth99001 <parthgandhi350@gmail.com>
2025-09-29 17:24:42 +0530

    adminpannel

--- adminPanel/index.js
+++ adminPanel/index.js
@@ -1,20 +1,23 @@
-const express = require("express");
-const app = express();
+const express = require("express")
+const PORT = 8080
+const app = express()
 const path = require("path")
 
-const PORT = 8080
-
 app.set("view engine", "ejs")
-app.use("./assets", express.static(path.join(__dirname, "/assets")))
+app.use("/assets", express.static(path.join(__dirname, "/assets")))
 
 app.get("/", (req,res) => {
     res.render("dashboard")
-})
+ })
+
+ app.get("/", (req,res) => {
+    res.render("/login")
+ })
 
 app.listen(PORT, (err) => {
-    if(err){
-        console.log("server is not running")
-        return
-    }
-    console.log("server is running", PORT)
+  if (err) {
+    console.log("server is not running")
+    return;
+  }
+  console.log("server is running ", PORT)
 })
