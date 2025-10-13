a367100e1533d0f53487e66a9bedce7ce86460ee
Parth99001 <parthgandhi350@gmail.com>
2025-10-13 16:01:28 +0530

    api

---
+++ api/index.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const { connectDB } = require("./config/db")
+const app = express()
+require('dotenv').config()/
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+
+
+app.listen(process.env.PORT, (err) => {
+    if(err){
+        console.log("server is not connected")
+    }
+    // connectDB()
+    console.log("server is connected", process.env.PORT)
+})
